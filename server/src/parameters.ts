d49b0229b680ff7a5dc7305ea107b32020fedde7
Sam <samnottheuncle@gmail.com>
2024-06-26 16:46:50 +0100

    Minor parameter correction

--- server/src/parameters.ts
+++ server/src/parameters.ts
@@ -6,4 +6,4 @@ export const PARAM_DATABASE_URL = "mongodb://127.0.0.1:27017/messagemedb";
 
 // Console logging. 0=disabled, 1=normal logging, 2=verbose debugging
 // NOTE: Setting this to anything other than 0 may significantly impact performance
-export const PARAM_LOGGING_LEVEL = 1;    // Change accordingly to your preference
+export const PARAM_LOGGING_LEVEL = 0;    // Change accordingly to your preference
